be458756553b9d962fb1af07cbadcfeabb189c97
Web4 <167559384+Web4application@users.noreply.github.com>
2025-05-06 23:02:49 -0700

    Update route.ts

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -8,7 +8,7 @@ export async function GET(request: Request) {
   const requestUrl = new URL(request.url);
   const code = requestUrl.searchParams.get("code");
   const origin = requestUrl.origin;
-  const redirectTo = requestUrl.searchParams.get("redirect_to")?.toString();
+  const redirectTo = requestUrl.searchParams.get("https://nextjs-with-supabase-gamma-khaki.vercel.app")?.toString();
 
   if (code) {
     const supabase = await createClient();
